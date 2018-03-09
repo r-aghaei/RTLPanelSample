17d44aeb9f5c0d458ff3be45c25aa73a937deba3
Reza Aghaei <rag@sitecore.net>
2018-03-09 15:52:25 +0800

    Changed folder astructure and updated readme.

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# WindowsFormsSample
-This project contains some Windows Forms examples.
+#RTL Panel
 
-## List of examples
+When creating RTL windows applications for languages like persian, arabic or hebrew, to have both text flow and control placements as right-tol-left, both `RightToLeftLayout` and `RightToLeft` properties usually are set to true. 
+This way also the form title bar will be mirrored and control box will be shown at left:
 
-- **ExPanel**  
-  An extended `Panel` with RTL support. Having `RihgtToLeftLayout` property.
-- **ExTextBox**  
-  A `TextBox` which can sow a hit in text area. Having a `Hint` property.
+![RTL Form](RTLWindow.png)
+
+But in most cases users are using LTR windows and having a windows which shows close button at left is not user-friendly for most  users.
+
+If you don't like to have right to left title bar and the control box at left, you should create your right to left container yourself and put controls in it and then set `RightToLeftLayout` of the container to true and set `RightToLeft` of the container to yes to have a complete right to left layout in the container without changing the layout of title bar and control box:
+
+![RTL Form](RTLPanel.png)
+
+To do so, we can add `WS_EX_LAYOUTRTL` and `WS_EX_NOINHERITLAYOUT` styles to the panel wehn craeting the panel:
+
+
+    protected override CreateParams CreateParams
+    {
+        get
+        {
+            CreateParams CP;
+            CP = base.CreateParams;
+            if (this.RightToLeftLayout &&
+                this.RightToLeft == System.Windows.Forms.RightToLeft.Yes)
+                CP.ExStyle = CP.ExStyle | WS_EX_LAYOUTRTL | WS_EX_NOINHERITLAYOUT;
+            return CP;
+        }
+    }
---
+++ src/WindowsFormsSample/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exPanel1 = new WindowsFormsSample.ExPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.exPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exPanel1
+            // 
+            this.exPanel1.Controls.Add(this.textBox2);
+            this.exPanel1.Controls.Add(this.textBox1);
+            this.exPanel1.Controls.Add(this.label2);
+            this.exPanel1.Controls.Add(this.label1);
+            this.exPanel1.Controls.Add(this.button2);
+            this.exPanel1.Controls.Add(this.button1);
+            this.exPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exPanel1.Location = new System.Drawing.Point(0, 0);
+            this.exPanel1.Name = "exPanel1";
+            this.exPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.exPanel1.RightToLeftLayout = true;
+            this.exPanel1.Size = new System.Drawing.Size(256, 111);
+            this.exPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(88, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "تایید";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(169, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "انصراف";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "رمز عبور:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "نام کاربری:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(75, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(75, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 111);
+            this.Controls.Add(this.exPanel1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "ورود";
+            this.exPanel1.ResumeLayout(false);
+            this.exPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ExPanel exPanel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
